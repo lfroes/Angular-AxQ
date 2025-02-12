79824cf23bd93aa31daef158d8dfc9fd0316753e
lfroes <nikowluiz@gmail.com>
2025-02-11 23:51:08 -0300

    feat(user-list): finished `search` on user list

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,8 +1,43 @@
 export interface User {
-  name: string;
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: string;
+  };
   email: string;
-  role: string;
-  lastSeen: string | Date | null;
-  online: boolean;
-  image: string;
+  login: {
+    uuid: string;
+    username: string;
+  };
+  dob: {
+    date: string;
+    age: number;
+  };
+  registered: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  id: {
+    name: string;
+    value: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
 }
--- src/app/shared/components/users-list/users-list.component.ts
+++ src/app/shared/components/users-list/users-list.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, switchMap, map } from 'rxjs';
 import { User } from '../../../models/user.model';
+
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
 })
 export class UsersListComponent {
+  private _users: User[] = [];
   private _searchQuery: string = '';
   private _filteredUsers: User[] = [];
-  private _displayedUsers: number = 3;
-
+  private _displayedUsers = new BehaviorSubject<number>(100);
 
   get filteredUsers(): User[] {
     return this._filteredUsers;
@@ -21,17 +27,55 @@ export class UsersListComponent {
   }
 
   set searchQuery(value: string) {
+    console.log('Search query changed:', value);
     this._searchQuery = value;
     this.filterUsers();
   }
 
   get hasMoreUsers(): boolean {
-    return this._displayedUsers < this._filteredUsers.length;
+    return this._displayedUsers.value < this._filteredUsers.length;
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this._displayedUsers
+      .pipe(
+        switchMap((displayedUsers) => {
+          const apiUrl = `https://randomuser.me/api/?results=${displayedUsers}`;
+          return this.http.get<{ results: User[] }>(apiUrl);
+        }),
+      )
+      .subscribe({
+        next: (response) => {
+          this._users = response.results;
+          this.filterUsers();
+        },
+        error: (error) => {
+          console.error('Error fetching users', error);
+        },
+      });
   }
 
   private filterUsers(): void {
-    this._filteredUsers =
+    console.log('Filtering users with query:', this._searchQuery);
+    // Filter users based on search query, maybe there is a better way to do this with rxjs
+    this._filteredUsers = this._users
+      .filter(
+        (user) =>
+          user.name.first
+            .toLowerCase()
+            .includes(this._searchQuery.toLowerCase()) ||
+          user.name.last
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase()) ||
+          user.email.toLowerCase().includes(this._searchQuery.toLowerCase()),
+      )
+      .slice(0, this._displayedUsers.value);
+    console.log('Filtered users:', this._filteredUsers.length);
   }
 
-  //TODO: Continue from here
+  loadMore(): void {
+    this._displayedUsers.next(this._displayedUsers.value + 100);
+  }
 }
